34fc5bbc5fa0fc87f6bcc22a620791c66122b7ad
Darshan98Solanki <darshan98solanki@gmail.com>
2025-01-25 12:03:23 +0530

    cahneg

--- common/src/index.ts
+++ common/src/index.ts
@@ -8,12 +8,18 @@ export const cakeSchema = zod.object({
 })
 
 export const updateCakeSchema = zod.object({
-    id: zod.string({ message: "Cake is id required" }),
-    name: zod.string({ message: "Cake is required" }).min(3, { message: "Cake name should be greater than 3 characters" }),
+    id: zod.string({ message: "Cake id is required" }),
+    name: zod.string({ message: "Cake name is required" }).min(3, { message: "Cake name should be greater than 3 characters" }),
     halfKgPrice: zod.string({ message: "Cake half kg price is required" }).min(1, { message: "Cake price should be greater than 0" }),
     oneKgPrice: zod.string({ message: "Cake one kg price is required" }).min(1, { message: "Cake price should be greater than 0" }),
     publicId: zod.string({ message: "Cake image id is required" })
 })
 
+export const removeCakeSchema = zod.object({
+    id: zod.string({ message: "Cake id is required" }),
+    publicId: zod.string({ message: "Cake image id is required" })
+})
+
 export type CakeSchema = zod.infer<typeof cakeSchema> 
 export type UpdateCakeSchema = zod.infer<typeof updateCakeSchema> 
+export type RemoveCakeSchema = zod.infer<typeof removeCakeSchema> 
